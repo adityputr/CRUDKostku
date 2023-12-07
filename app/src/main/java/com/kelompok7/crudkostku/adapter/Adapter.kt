09500c097cc7c25c865632e0657a8465d278fd6f
Rachmad Yoga Saputra <yogas910@gmail.com>
2023-12-07 20:50:08 +0700

    menampilkan image di itemview

--- app/src/main/java/com/kelompok7/crudkostku/adapter/Adapter.kt
+++ app/src/main/java/com/kelompok7/crudkostku/adapter/Adapter.kt
@@ -3,10 +3,10 @@ package com.kelompok7.crudkostku.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.content.contentValuesOf
 import androidx.recyclerview.widget.RecyclerView
 import com.kelompok7.crudkostku.databinding.KostItemBinding
 import com.kelompok7.crudkostku.models.Kost
+import com.squareup.picasso.Picasso
 
 class Adapter(private val kostList : java.util.ArrayList<Kost>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
     class ViewHolder(val binding: KostItemBinding) : RecyclerView.ViewHolder(binding.root){
@@ -28,6 +28,7 @@ class Adapter(private val kostList : java.util.ArrayList<Kost>) : RecyclerView.A
                 tvNama.text = currentItem.nama
                 tvAlamat.text = currentItem.alamat
                 tvId.text = currentItem.id
+                Picasso.get().load(currentItem.imgUrl).into(tvImage)
             }
         }
     }
