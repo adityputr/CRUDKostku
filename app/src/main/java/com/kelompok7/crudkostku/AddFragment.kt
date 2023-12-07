4615d5cbec97ef71f7f574665f3ad8e6f931e65d
Rachmad Yoga Saputra <yogas910@gmail.com>
2023-12-07 20:21:19 +0700

    Menambahkan button pick image pada AddFragment, untuk bisa diupload ke realtime database

--- app/src/main/java/com/kelompok7/crudkostku/AddFragment.kt
+++ app/src/main/java/com/kelompok7/crudkostku/AddFragment.kt
@@ -1,33 +1,46 @@
 package com.kelompok7.crudkostku
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.kelompok7.crudkostku.databinding.FragmentAddBinding
-import com.kelompok7.crudkostku.databinding.FragmentHomeBinding
 import com.kelompok7.crudkostku.models.Kost
 
 class AddFragment : Fragment() {
     private var _binding : FragmentAddBinding? = null
     private val binding get() = _binding!!
     private lateinit var firebaseRef : DatabaseReference
+    private lateinit var storageRef : StorageReference
 
+    private var uri: Uri? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentAddBinding.inflate(inflater, container, false)
         firebaseRef = FirebaseDatabase.getInstance().getReference("Kost")
-
+        storageRef = FirebaseStorage.getInstance().getReference("Images")
         binding.btnSend.setOnClickListener {
             saveData()
         }
-
+        val picImage = registerForActivityResult(ActivityResultContracts.GetContent()){
+            binding.imageAdd.setImageURI(it)
+            if (it != null){
+                uri = it
+            }
+        }
+        binding.btnPickImage.setOnClickListener {
+            picImage.launch("image/*")
+        }
         return binding.root
     }
 
@@ -35,18 +48,33 @@ class AddFragment : Fragment() {
         val nama = binding.editNamaKos.text.toString()
         val alamat = binding.editAlamatKos.text.toString()
 
+
         if (nama.isEmpty()) binding.editNamaKos.error = "Masukan nama kos!"
         if (alamat.isEmpty()) binding.editAlamatKos.error = "Masukan alamat kos!"
 
-        val kostId = firebaseRef.push().key!!
-        val kost = Kost(kostId, nama, alamat)
 
-        firebaseRef.child(kostId).setValue(kost)
-            .addOnCompleteListener{
-                Toast.makeText(context, "Data berhasil ditambahkan!", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener{
-                Toast.makeText(context, "Data gagal ditambahkan!${it.message}", Toast.LENGTH_SHORT).show()
-            }
+        val kostId = firebaseRef.push().key!!
+        var kost : Kost
+
+        uri?.let {
+            storageRef.child(kostId).putFile(it)
+                .addOnSuccessListener { task->
+                    task.metadata!!.reference!!.downloadUrl
+                        .addOnSuccessListener { url ->
+                            val imgUrl = url.toString()
+
+                            kost = Kost( kostId, nama, alamat, imgUrl)
+
+                            firebaseRef.child(kostId).setValue(kost)
+                                .addOnCompleteListener{
+                                    Toast.makeText(context, "Data berhasil ditambahkan!", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener{
+                                    Toast.makeText(context, "Data gagal ditambahkan!${it.message}", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                }
+        }
+
     }
 }
--- app/src/main/java/com/kelompok7/crudkostku/models/Kost.kt
+++ app/src/main/java/com/kelompok7/crudkostku/models/Kost.kt
@@ -3,5 +3,6 @@ package com.kelompok7.crudkostku.models
 data class Kost(
     val id : String? = null,
     val nama : String? = null,
-    val alamat : String? = null
+    val alamat : String? = null,
+    val imgUrl: String? = null
 )
