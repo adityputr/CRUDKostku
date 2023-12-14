c934c40b5d033e9689c2ffd6f0bfff143def8b73
adityputr <adityputr45@gmail.com>
2023-12-14 20:09:37 +0700

    Menambahkan remove image

--- app/src/main/java/com/kelompok7/crudkostku/UpdateFragment.kt
+++ app/src/main/java/com/kelompok7/crudkostku/UpdateFragment.kt
@@ -1,17 +1,24 @@
 package com.kelompok7.crudkostku
 
+import android.app.ProgressDialog.show
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.kelompok7.crudkostku.databinding.FragmentUpdateBinding
 import com.kelompok7.crudkostku.models.Kost
+import com.squareup.picasso.Picasso
 
 class UpdateFragment : Fragment() {
 
@@ -21,6 +28,10 @@ class UpdateFragment : Fragment() {
     private val args : UpdateFragmentArgs by navArgs()
 
     private lateinit var firebaseRef : DatabaseReference
+    private lateinit var storageRef : StorageReference
+
+    private var uri: Uri? = null
+    private var imageUrl : String? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,14 +39,42 @@ class UpdateFragment : Fragment() {
         _binding = FragmentUpdateBinding.inflate(inflater, container,false)
 
         firebaseRef = FirebaseDatabase.getInstance().getReference("Kost")
+        storageRef = FirebaseStorage.getInstance().getReference("Images")
+
+        val picImage = registerForActivityResult(ActivityResultContracts.GetContent()){
+            binding.imgUpdate.setImageURI(it)
+            if (it != null){
+                uri = it
+            }
+        }
+        imageUrl = args.imageUrl
 
         binding.apply {
             editUpdateNama.setText(args.nama)
             editUpdateAlamat.setText(args.alamat)
+            Picasso.get().load(imageUrl).into(imgUpdate)
+
             btnUpdate.setOnClickListener{
                 updateData()
                 findNavController().navigate(R.id.action_updateFragment_to_homeFragment)
             }
+            imgUpdate.setOnClickListener{
+                context?.let { context ->
+                    MaterialAlertDialogBuilder(context)
+                        .setTitle("Ganti foto")
+                        .setMessage("Apakah anda yakin ingin mengganti?")
+                        .setPositiveButton("Ganti gambar"){_,_->
+                            picImage.launch("image/*")
+
+                        }
+                        .setNegativeButton("Hapus gambar"){_,_->
+                            imageUrl = null
+                            binding.imgUpdate.setImageResource(R.drawable.ic_sponbob)
+                        }
+                        .setNeutralButton("Batal"){_,_->}
+                            .show()
+                        }
+                }
         }
 
         return binding.root
@@ -44,14 +83,52 @@ class UpdateFragment : Fragment() {
     private fun updateData() {
         val nama = binding.editUpdateNama.text.toString()
         val alamat = binding.editUpdateAlamat.text.toString()
-        val kost = Kost(args.id, nama, alamat)
+        var kost: Kost
 
-        firebaseRef.child(args.id).setValue(kost)
-            .addOnCompleteListener{
-                Toast.makeText(context, "Data berhasil diupdate!", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener{
-                Toast.makeText(context, "Data gagal diupdate!${it.message}", Toast.LENGTH_SHORT).show()
+        if (uri != null) {
+            storageRef.child(args.id).putFile(uri!!)
+                .addOnSuccessListener { task ->
+                    task.metadata!!.reference!!.downloadUrl
+                        .addOnSuccessListener { url ->
+                            imageUrl = url.toString()
+                            kost = Kost(args.id, nama, alamat, imageUrl)
+                            firebaseRef.child(args.id).setValue(kost)
+                                .addOnCompleteListener {
+                                    Toast.makeText(
+                                        context,
+                                        "Data berhasil diupdate!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(
+                                        context,
+                                        "Data gagal diupdate!${it.message}",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                        }
+                }
+
         }
+        if (uri == null){
+            kost = Kost(args.id, nama, alamat, imageUrl)
+            firebaseRef.child(args.id).setValue(kost)
+                .addOnCompleteListener {
+                    Toast.makeText(
+                        context,
+                        "Data berhasil diupdate!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(
+                        context,
+                        "Data gagal diupdate!${it.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        }
+        if (imageUrl == null) storageRef.child(args.id).delete()
     }
 }
--- app/src/main/java/com/kelompok7/crudkostku/adapter/Adapter.kt
+++ app/src/main/java/com/kelompok7/crudkostku/adapter/Adapter.kt
@@ -3,8 +3,11 @@ package com.kelompok7.crudkostku.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.database.FirebaseDatabase
 import com.kelompok7.crudkostku.HomeFragmentDirections
 import com.kelompok7.crudkostku.databinding.KostItemBinding
 import com.kelompok7.crudkostku.models.Kost
@@ -36,9 +39,36 @@ class Adapter(private val kostList : java.util.ArrayList<Kost>) : RecyclerView.A
                     val action = HomeFragmentDirections.actionHomeFragmentToUpdateFragment(
                         currentItem.id.toString(),
                         currentItem.nama.toString(),
-                        currentItem.alamat.toString()
+                        currentItem.alamat.toString(),
+                        currentItem.imgUrl.toString()
                     )
                     findNavController(holder.itemView).navigate(action)
+                }
+
+                rvContainer.setOnLongClickListener {
+                    MaterialAlertDialogBuilder(holder.itemView.context)
+                        .setTitle("Hapus item secara permanen")
+                        .setMessage("Apakah kamu yakin ingin menghapus item ini?")
+                        .setPositiveButton("Yes"){_,_ ->
+                            val firebaseRef = FirebaseDatabase.getInstance().getReference("Kost")
+                            val storageRef = FirebaseDatabase.getInstance().getReference("Images")
+                            //storage
+                            storageRef.child(currentItem.id.toString()).removeValue()
+
+                            //realtime database
+                            firebaseRef.child(currentItem.id.toString()).removeValue()
+                                .addOnSuccessListener {
+                                    Toast.makeText(holder.itemView.context, "Item berhasil dihapus!", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener{error ->
+                                    Toast.makeText(holder.itemView.context, "error ${error.message}", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                        .setNegativeButton("No"){_,_ ->
+                            Toast.makeText(holder.itemView.context, "Dibatalkan!", Toast.LENGTH_SHORT).show()
+                        }
+                        .show()
+                    return@setOnLongClickListener true
                 }
             }
         }
